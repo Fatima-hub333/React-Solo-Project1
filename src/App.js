330379c22d42bd9561013a90aee096dfacab2f61
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-14 20:47:02 +0500

    Update files & remove issues

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Info from './components/Info'
-import About from './components/About/About'
+import About from './components/About'
+import './index.css'
 
 
 export default function App() {
   return (
-    <div>
+    <div className="container">
       <Info />
       <About />
     </div>
--- src/components/Info.js
+++ src/components/Info.js
@@ -6,13 +6,13 @@ import linkedin from './images/buttons/linkedin.svg'
 
 export default function Information() {
   return (
-    <div className="app">
       <div className="Wrapper">
         <img src={image}  alt="Profile" />
         <div className="contact-section">
           <div className="cardBody">
             <h1>Fatima Zahir</h1>
-            <h4>Full Stack Developer</h4>
+          <h3>Full Stack Developer</h3>
+          <h4>fatimaazahir.website</h4>
             <div className="buttons">
               <a href='fatimaazahir@gmail.com'> <img className="email" src={email} alt="Email Button" /> </a>
               <a href='https://www.linkedin.com/in/fatima-z-181583234/'> <img className="linkedin" src={linkedin} alt="Linkedin Button" /> </a>
@@ -20,6 +20,5 @@ export default function Information() {
           </div>
         </div>
       </div>
-    </div>
   )
 }
